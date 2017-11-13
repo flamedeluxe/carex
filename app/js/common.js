5896befc2d2b102e30b7450e338ace6ee5d09655
Andrey <flamedeluxe@yandex.ru>
2017-11-13 13:17:28 +0800

    map

--- app/js/common.js
+++ app/js/common.js
@@ -8,3 +8,34 @@ $(function() {
 	});
 
 });
+
+yamap(60.967595, 69.045735);
+		
+function yamap(lat, lng) {
+	ymaps.ready(init);
+ 
+    function init() {
+        var myMap = new ymaps.Map("map", {
+            center: [lat, lng],
+            zoom: 18,
+            controls: []
+        });
+        var myPlacemark = new ymaps.Placemark(myMap.getCenter(),{}, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/marker.svg',
+            iconImageSize: [50, 60],
+            iconImageOffset: [0, 0]
+        });
+        myMap.behaviors.disable(['scrollZoom', 'rightMouseButtonMagnifier'])                    
+        myMap.geoObjects.add(myPlacemark);
+    }
+}
+
+
+$('body').on('click', '#markers li', function() {
+	var lat = parseFloat($(this).attr('data-lat'));
+	var lng = parseFloat($(this).attr('data-lng'));
+	$('#map').html('');
+	yamap(lat, lng);
+	return false;
+});
